af329717b88a541c46329ad8ce313dc606a2b38c
Matthew J Cloyd <cloyd.matt@gmail.com>
2020-10-17 11:17:08 -0400

    Warn about custom statements

--- src/components/Form.js
+++ src/components/Form.js
@@ -2,7 +2,17 @@ import React from 'react'
 import axios from 'axios';
 
 export default class Form extends React.Component {
-  state = { aspen: "(Liz) [knows] (Jack)." };
+  state = {
+    aspen: "(Liz) [knows] (Jack).",
+    match_present: false
+  };
+
+  onChange = (event) => {
+    const regex = new RegExp(/^grammar:/, 'gm')
+    const source = event.target.value
+    const match_present = regex.test(source)
+    this.setState({ aspen: source, match_present: match_present })
+  }
 
   handleSubmit = async (event) => {
     const api_url = process.env.API_HOST || 'http://localhost:9292'
@@ -20,10 +30,17 @@ export default class Form extends React.Component {
           rows="8"
           type="text"
           value={this.state.aspen}
-          onChange={event => this.setState({ aspen: event.target.value })}
+          onChange={this.onChange}
           required
         />
         <br />
+        {this.state.match_present &&
+          <p>
+            Custom grammars will only appear in the Cypher export.
+            Click "Copy Cypher" after converting Aspen in order
+            to see the results of the custom grammar.
+          </p>
+        }
         <button>Convert Aspen</button>
       </form>
     );
